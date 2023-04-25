318de7dafb783c1e9aedaa2e7c2772081ce78b8e
Philippe Virouleau <philippe.44@gmail.com>
2023-04-25 13:29:47 +0200

    Init fullcalendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Competition } from '@wca/helpers';
 
 import './App.css';
@@ -6,10 +6,25 @@ import PersonalSchedules from './components/PersonalSchedules';
 
 
 function App() {
-  const wcif = require("./wcif.json") as Competition;
+  //const wcif = require("./wcif.json") as Competition;
+  const id = "FrenchChampionship2023";
+  const [wcif, setWcif] = useState(undefined);
+  useEffect(() => {
+    console.log("coucou");
+    fetch(`https://staging.worldcubeassociation.org/api/v0/competitions/${id}/wcif/public`)
+      .then(response => response.json())
+      .then(setWcif)
+      .catch(error => console.log(error));
+  }, [id])
   return (
     <div className="App">
-      <PersonalSchedules wcif={wcif} />
+      {wcif ? (
+        <PersonalSchedules wcif={wcif} />
+      ) : (
+        <div>
+          No WCIF yet.
+        </div>
+      )}
     </div>
   );
 }
--- src/components/PersonalSchedules.tsx
+++ src/components/PersonalSchedules.tsx
@@ -1,6 +1,11 @@
 import React, { useMemo } from 'react';
 
-import { Competition, Schedule, Activity, Room } from '@wca/helpers';
+import { Assignment, Competition, Schedule, Activity, Room } from '@wca/helpers';
+
+import FullCalendar from '@fullcalendar/react';
+import listPlugin from '@fullcalendar/list';
+
+import './calendar.css';
 
 interface ActivityWithRoom extends Activity {
   // FIXME: figure out if this does duplicate room data or if we get a ref
@@ -28,33 +33,139 @@ function computeActivitiesById(schedule: Schedule): ActivitiesById {
   return activitiesById;
 }
 
+type CalendarProps = {
+  activitiesById: ActivitiesById;
+  assignments: Assignment[];
+  views: any;
+  date: string;
+};
+
+const codeToShort: { [key: string]: string } = {
+  "competitor": "C",
+  "staff-scrambler": "S",
+  "staff-judge": "J",
+  "staff-runner": "R",
+};
+
+function activityToEvent(assignment: Assignment,
+  activitiesById: ActivitiesById) {
+  const { activityId, assignmentCode } = assignment;
+  const activity = activitiesById[activityId];
+  return {
+    id: `${activity.id}`,
+    title: `${activity.activityCode} - ${codeToShort[assignmentCode] || assignmentCode}`,
+    color: activity.room.color,
+    start: activity.startTime,
+    end: activity.endTime,
+  };
+}
+
+function Cal({ activitiesById, views, date, assignments } : CalendarProps) {
+  const events = assignments.map(a => activityToEvent(a, activitiesById));
+  const eventsByDate: { [key: string]: any[] } = {
+  };
+  events.forEach(e => {
+    // FIXME: something reliable that works in puppeteer.
+    const date = e.start.slice(0, 10);
+    eventsByDate[date] ||= [];
+    eventsByDate[date].push(e);
+  });
+
+  const titi = Object.keys(eventsByDate);
+
+  // FIXME: set timezone according to the room's WCIF
+  return (
+    <>
+      {titi.map((d: string) => {
+        return (
+          <FullCalendar
+            key={d}
+            plugins={[listPlugin]}
+            initialDate={d}
+            initialView='compList'
+            headerToolbar={false}
+            displayEventEnd={false}
+            contentHeight='auto'
+            eventTimeFormat={{
+              hour12: false,
+              hour: '2-digit',
+              minute: '2-digit',
+            }}
+            views={views}
+            initialEvents={eventsByDate[d]}
+          />
+        );
+      })}
+    </>
+  );
+}
+
 type PSProps = {
   wcif: Competition;
 };
 
 export default function PersonalSchedules({ wcif } : PSProps) {
-  const { name } = wcif;
-  const p = wcif.persons[0];
-  p.assignments ||= [];
+  const sorted = wcif.persons.sort((a, b) => a.name.localeCompare(b.name));
+  const persons = sorted.slice(0, 10);
+  const persons1 = sorted.slice(10, 20);
+  const persons2 = sorted.slice(20, 30);
   const activitiesById: ActivitiesById = useMemo(
     () => computeActivitiesById(wcif.schedule),
     [wcif]
   );
-  console.log(activitiesById);
+  const views = {
+    compList: {
+      type: 'list',
+      duration: { days: wcif.schedule.numberOfDays },
+    },
+  };
   return (
-    <>
-      <div>Personal schedules for {name}.</div>
-      <ul>
-        {p.assignments.map(a => {
-          const activity = activitiesById[a.activityId];
-          console.log(a, activity);
+    <div className="row">
+      <div className="cal-column">
+        {persons.map(({ wcaId, name, assignments }) => {
           return (
-            <li key={a.activityId}>
-              {a.assignmentCode}: {activitiesById[a.activityId].startTime}
-            </li>
+            <div key={wcaId} className="cal">
+              <p className="name">{name}</p>
+              <Cal
+                activitiesById={activitiesById}
+                assignments={assignments || []}
+                views={views}
+                date={wcif.schedule.startDate}
+              />
+            </div>
           );
         })}
-      </ul>
-    </>
+      </div>
+      <div className="cal-column">
+        {persons1.map(({ wcaId, name, assignments }) => {
+          return (
+            <div key={wcaId} className="cal">
+              <p className="name">{name}</p>
+              <Cal
+                activitiesById={activitiesById}
+                assignments={assignments || []}
+                views={views}
+                date={wcif.schedule.startDate}
+              />
+            </div>
+          );
+        })}
+      </div>
+      <div className="cal-column">
+        {persons2.map(({ wcaId, name, assignments }) => {
+          return (
+            <div key={wcaId} className="cal">
+              <p className="name">{name}</p>
+              <Cal
+                activitiesById={activitiesById}
+                assignments={assignments || []}
+                views={views}
+                date={wcif.schedule.startDate}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
